04bb35fe72a21db5be1522b1aa1b79b5df107946
Ahmed Elbohoty <ahmed.elbohoty@zid.sa>
2025-11-19 18:36:53 +0200

    chore: implement 'AppIconWithTooltip' component

---
+++ src/components/app-icon-with-tooltip.tsx
@@ -0,0 +1,51 @@
+import Box from '@mui/material/Box';
+import { SvgIconOwnProps } from '@mui/material/SvgIcon';
+import Tooltip, { TooltipProps } from '@mui/material/Tooltip';
+import { styled } from '@mui/material/styles';
+
+import { IconInformationLine } from '../icons/system/information-line';
+
+import { StackRow } from './stack-row';
+
+//
+//
+
+// NOTE: accessibleAreaWidth and accessibleAreaHeight are used to create non-visible area for
+//       the tooltip to be accessible on mobile
+
+type AppIconWithTooltipProps = Omit<TooltipProps, 'children'> & {
+  accessibleAreaWidth?: string;
+  accessibleAreaHeight?: string;
+  icon?: React.ReactNode;
+  iconProps?: SvgIconOwnProps;
+};
+
+export const AppIconWithTooltip: React.FC<AppIconWithTooltipProps> = ({
+  title,
+  accessibleAreaWidth = '36px',
+  accessibleAreaHeight = '36px',
+  icon,
+  iconProps,
+  ...props
+}) => {
+  return (
+    <Tooltip title={title} placement="top" {...props}>
+      <StackRow position="relative" alignItems="center" justifyContent="center">
+        {icon ?? <IconInformationLine fontSize="inherit" color="action" {...iconProps} />}
+        <TooltipArea width={accessibleAreaWidth} height={accessibleAreaHeight} />
+      </StackRow>
+    </Tooltip>
+  );
+};
+
+const TooltipArea = styled(Box)(({ width, height }) => ({
+  position: 'absolute',
+  display: 'none',
+  pointerEvents: 'none',
+  '@media (pointer:none), (pointer:coarse), (hover:none)': {
+    display: 'block',
+    width: width || '24px',
+    height: height || '24px',
+    pointerEvents: 'auto',
+  },
+}));
---
+++ src/stories/AppIconWithTooltip.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { AppIconWithTooltip } from '../components/app-icon-with-tooltip';
+
+//
+//
+
+// Meta
+const meta = {
+  title: 'App Icon With Tooltip',
+  component: AppIconWithTooltip,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    title: {
+      control: 'text',
+    },
+    accessibleAreaWidth: {
+      control: 'text',
+    },
+    accessibleAreaHeight: {
+      control: 'text',
+    },
+    iconProps: {
+      control: 'object',
+    },
+  },
+} satisfies Meta<typeof AppIconWithTooltip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+//
+
+export const Variants: Story = {
+  args: {
+    title: 'Tooltip sample',
+    accessibleAreaWidth: '36px',
+    accessibleAreaHeight: '36px',
+  },
+};
