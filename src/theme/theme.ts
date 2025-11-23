6e51b94c062131552f9891bc52b9672544a5c75b
Ahmed Elbohoty <ahmed.elbohoty@zid.sa>
2025-11-23 10:29:11 +0200

    chore: add mui/lab and update types.d.ts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,13 +2,19 @@ import { createTheme } from '@mui/material/styles';
 
 import { components } from './components';
 import { palette } from './palette';
+import { shadows } from './shadows';
 import { typography } from './typography';
 
 //
 //
 
 export const themeParcel = createTheme({
+  shape: {
+    borderRadius: 8,
+    standardBorderRadius: 12,
+  },
   typography,
   components,
   palette,
+  shadows,
 });
--- src/theme/types.d.ts
+++ src/theme/types.d.ts
@@ -1,7 +1,5 @@
 import '@mui/material';
 
-//
-
 declare module '@mui/material/Button' {
   interface ButtonPropsColorOverrides {
     primaryDark: true;
@@ -10,9 +8,231 @@ declare module '@mui/material/Button' {
   }
 }
 
-//
+declare module '@mui/material/IconButton' {
+  interface IconButtonPropsSizeOverrides {
+    extraSmall: true;
+  }
+}
+declare module '@mui/material/Radio' {
+  interface RadioPropsSizeOverrides {
+    small: true;
+    medium: true;
+    large: true;
+  }
+}
+
+declare module '@mui/lab' {
+  interface TimelineProps {
+    noOppositeContent?: boolean;
+  }
+}
 
 declare module '@mui/material/styles' {
+  interface CustomPalette {
+    badge: {
+      primary: {
+        color: '#8758C6';
+        backgroundColor: '#F1EAFF';
+      };
+    };
+    outlined?: {
+      outlined?: string;
+      'outlined 2'?: string;
+      outlinedDisable?: string;
+    };
+    tabs: {
+      text: {
+        secondary: string;
+      };
+    };
+    icon: {
+      primary: string;
+      secondary: string;
+      disabled: string;
+      draggable: string;
+    };
+    _states: {
+      focus: string;
+      hover: string;
+      selected: string;
+      focusVisible: string;
+      outlinedBorder: string;
+    };
+
+    _components: {
+      sliderIndicator: {
+        active: string;
+        inactive: string;
+        background: string;
+      };
+      cardContent: {
+        selected: {
+          background: string;
+          border: string;
+        };
+      };
+      emptyState: {
+        imageBackground: string;
+      };
+      navbar: {
+        wallet: {
+          background: string;
+          border: string;
+          text: string;
+        };
+        search: {
+          searchBorder: string;
+          searchBackground: string;
+        };
+      };
+      avatar: {
+        fill: string;
+      };
+      input: {
+        _states: {
+          hover: string;
+          selected: string;
+          focusVisible: string;
+        };
+        outlined: {
+          enabledBorder: string;
+          hoverBorder: string;
+        };
+      };
+      switch: {
+        knobFillEnabled: string;
+        slideFill: string;
+        knowFillDisabled: string;
+        shadow: string;
+        color: string;
+        background: string;
+        primary: {
+          activeBackground: string;
+          pressedBackground: string;
+          shadow: string;
+        };
+        error: {
+          activeBackground: string;
+          pressedBackground: string;
+          shadow: string;
+        };
+      };
+      chip: {
+        desert: string;
+      };
+      backdrop: {
+        fill: string;
+      };
+      appBar: {
+        defaultFill: string;
+      };
+      alert: {
+        error: {
+          color: string;
+          border: string;
+          background: string;
+        };
+        warning: {
+          color: string;
+          border: string;
+          background: string;
+        };
+        info: {
+          color: string;
+          border: string;
+          background: string;
+        };
+        success: {
+          color: string;
+          border: string;
+          background: string;
+        };
+      };
+      stepper: {
+        connector: string;
+      };
+      button: {
+        disabledText: string;
+        disabledBorder: string;
+        primaryDarkText: {
+          disabled: string;
+          text: string;
+          loadingText: string;
+        };
+        primary: {
+          contained?: string;
+          hover: string;
+          focused: string;
+          pressed: string;
+          disabled: string;
+          border: string;
+          text: string;
+          loadingText: string;
+        };
+        secondary: {
+          contained?: string;
+          hover: string;
+          focused: string;
+          pressed: string;
+          disabled: string;
+          border: string;
+          text: string;
+          loadingText: string;
+        };
+        tertiary: {
+          contained: string;
+          hover: string;
+          focused: string;
+          pressed: string;
+          disabled: string;
+          text: string;
+          outlinedHover: string;
+          outlinedText: string;
+          loadingText: string;
+        };
+        error: {
+          contained?: string;
+          hover: string;
+          focused: string;
+          pressed: string;
+          disabled: string;
+          border: string;
+          text: string;
+          outlinedHover: string;
+          outlinedText: string;
+          activeBorder: string;
+          loadingText: string;
+        };
+      };
+      iconButton: {
+        default: {
+          hover: string;
+        };
+        primary: {
+          text: string;
+          focused: string;
+          hover: string;
+          focusedIcon: string;
+        };
+        secondary: {
+          text: string;
+          focused: string;
+          hover: string;
+          focusedIcon: string;
+        };
+        error: {
+          text: string;
+          focused: string;
+          hover: string;
+          focusedIcon: string;
+        };
+      };
+    };
+    _native: {
+      'scrollbar-bg': string;
+    };
+  }
+
   interface PaletteOptions extends CustomPalette {}
 
   interface Palette extends CustomPalette {}
@@ -39,71 +259,86 @@ declare module '@mui/material/styles' {
 
   //
 
-  interface _Components {
-    button: {
-      disabledText: string;
-      disabledBorder: string;
-      primaryDarkText: {
-        disabled: string;
-        text: string;
-        loadingText: string;
-      };
-      primary: {
-        contained?: string;
-        hover: string;
-        focused: string;
-        pressed: string;
-        disabled: string;
-        border: string;
-        text: string;
-        loadingText: string;
-      };
-      secondary: {
-        contained?: string;
-        hover: string;
-        focused: string;
-        pressed: string;
-        disabled: string;
-        border: string;
-        text: string;
-        loadingText: string;
-      };
-      tertiary: {
-        contained: string;
-        hover: string;
-        focused: string;
-        pressed: string;
-        disabled: string;
-        text: string;
-        outlinedHover: string;
-        outlinedText: string;
-        loadingText: string;
-      };
-      error: {
-        contained?: string;
-        hover: string;
-        focused: string;
-        pressed: string;
-        disabled: string;
-        border: string;
-        text: string;
-        outlinedHover: string;
-        outlinedText: string;
-        activeBorder: string;
-        loadingText: string;
-      };
-    };
-  }
-
-  //
-
   interface Palette {
     colors: Colors;
-    _components: _Components;
   }
 
   interface PaletteOptions {
     colors: Colors;
-    _components: _Components;
+  }
+
+  //
+  interface TypeText {
+    tertiary: string;
+    deepGreen: string;
+    darkOchre: string;
+    contrastText: string;
+    primaryContrastTest: string;
+    _states: {
+      focusVisible: string;
+    };
+  }
+
+  interface CommonColors {
+    black_states: {
+      hover: string;
+      selected: string;
+      focus: string;
+    };
+    white_states: {
+      main: string;
+      focusVisible: string;
+      focus: string;
+    };
+  }
+
+  interface TypeBackground {
+    white: string;
+    black: string;
+    gray: string;
+    gray200: string;
+    gray100: string;
+    backgroundDisable: string;
+    darkBackdrop: string;
+    Blur: string;
+    green: string;
+    yellow: string;
+    royal: string;
+    mintGreen: string;
+    goldenYellow: string;
+    lavenderGlow: string;
+    royalPurple: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    _states?: {
+      hover?: string;
+      selected?: string;
+      focus?: string;
+      focusVisible?: string;
+      outlinedBorder?: string;
+    };
+  }
+  interface PaletteColor {
+    _states?: {
+      hover?: string;
+      selected?: string;
+      focus?: string;
+      focusVisible?: string;
+      outlinedBorder?: string;
+    };
+  }
+
+  interface Theme {
+    shape: {
+      borderRadius: number;
+      standardBorderRadius: number;
+    };
+  }
+  interface ThemeOptions {
+    shape?: {
+      borderRadius: number;
+      standardBorderRadius: number; // Add custom property to options as well
+    };
   }
 }
